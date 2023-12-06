f54ceb0b9508594d764f87cef3db62b91bd2a749
minsu1206 <minsumunsu@naver.com>
2023-12-06 13:33:12 +0900

    fix full.sh

--- full1.sh
+++ full1.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=cut_roasted_beef num_steps=1 data_downsample=4
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=cook_spinach num_steps=1 data_downsample=4
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=coffee_martini num_steps=1 data_downsample=4
+
 # training
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=cut_roasted_beef 
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=cut_roasted_beef 
 PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=cook_spinach 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=coffee_martini
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=coffee_martini
 
 # render
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=cut_roasted_beef 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=cook_spinach 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=coffee_martini 
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=cut_roasted_beef log-dir=logs/baseline/cut_roasted_beef
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=cook_spinach log-dir=logs/baseline/cook_spinach
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=coffee_martini log-dir=logs/baseline/coffee_martini
 
 # validate-only
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=cut_roasted_beef 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=cook_spinach 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=coffee_martini 
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=cut_roasted_beef log-dir=logs/baseline/cut_roasted_beef
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=cook_spinach log-dir=logs/baseline/cook_spinach
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=coffee_martini log-dir=logs/baseline/coffee_martini
 
 # spacetime-only
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=cut_roasted_beef 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=cook_spinach 
-PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=coffee_martini
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=cut_roasted_beef log-dir=logs/baseline/cut_roasted_beef
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=cook_spinach log-dir=logs/baseline/cook_spinach
+# PYTHONPATH='.' python plenoxels/main.py --device 0 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=coffee_martini log-dir=logs/baseline/coffee_martini
 
 
--- full2.sh
+++ full2.sh
@@ -6,18 +6,16 @@ PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/confi
 PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py expname=flame_steak
 
 # render
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=sear_steak 
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=flame_salmon_1 
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=flame_steak
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=sear_steak log-dir=logs/baseline/sear_steak
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=flame_salmon_1 log-dir=logs/baseline/flame_salmon_1
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --render-only expname=flame_steak log-dir=logs/baseline/flame_steak
 
 # validate-only
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=sear_steak 
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=flame_salmon_1
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=flame_steak
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=sear_steak log-dir=logs/baseline/sear_steak
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=flame_salmon_1 log-dir=logs/baseline/flame_salmon_1
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --validate-only expname=flame_steak log-dir=logs/baseline/flame_steak
 
 # spacetime-only
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=sear_steak 
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=flame_salmon_1
-PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=flame_steak
-
-
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=sear_steak log-dir=logs/baseline/sear_steak
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=flame_salmon_1 log-dir=logs/baseline/flame_salmon_1
+PYTHONPATH='.' python plenoxels/main.py --device 1 --config-path plenoxels/configs/final/DyNeRF/dynerf_hybrid.py --spacetime-only expname=flame_steak log-dir=logs/baseline/flame_steak
